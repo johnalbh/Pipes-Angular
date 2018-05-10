49eaa16e5b638b72ec1a9c80898419cd278fa5fc
Jairo Andres Lopez Hernandez <jairoandreslopezhernandez@MacBook-Air-de-Jairo.local>
2018-05-10 18:23:56 -0500

    Desarrollo de todos los Pipes por defecto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { deflateRaw } from 'zlib';
 
 @Component({
   selector: 'app-root',
@@ -7,7 +8,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   nombre = 'John López';
+  nombre2 = 'john albErto loPez hernandez';
   arreglo = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
   PI = Math.PI;
   a:number = 0.234;
+  salario: number = 1234.5;
+  producto = {
+    nombre: 'Pizza',
+    precio: 2500,
+    categoria: 'Emsambles',
+    ingredientes: {
+      queso: 'Mozarella',
+      jamon: 'Serrano'
+    }
+  };
+  valorDePromesa = new Promise( (resolve, reject) => {
+      setTimeout( () => resolve('Llego la data!'), 3500);
+  });
+  fecha = new Date();
+  video = 'lLOcKqAgcmI';
+  activar: boolean = true;
 }
